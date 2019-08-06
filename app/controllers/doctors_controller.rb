4a6d232ff1fb59d38b3cab9580cb9c9be0e54d99
“Mark <mkanko66@gmail.com>
2019-08-05 18:14:20 -0700

    completed displaying-has-many-through-rails-lab

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,26 @@
+class DoctorsController < ApplicationController
+
+    def index
+        @doctors = Doctor.all 
+    end 
+
+    def new
+        @doctor = Doctor.new
+    end 
+
+    def show
+        @doctor = Doctor.find(params[:id])
+    end
+
+    def create
+        @doctor = Doctor.new(doctors_params)
+        @doctor.save
+        redirect_to doctor_path(@doctor) 
+    end 
+
+    private
+
+    def doctors_params
+        params.require(:doctor).permit(:name, :department)
+    end 
+end
