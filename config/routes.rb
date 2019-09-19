67ca0f0260ee8d03d443f1485e5fda657e69e856
Kat Maldon <katharinamaldon@gmail.com>
2019-09-19 18:30:44 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'appoitnments/appointment_datetime'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors
   resources :patients
